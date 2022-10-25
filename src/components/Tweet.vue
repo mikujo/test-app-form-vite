d92711f0a536b35ec7b126f027571f36f91b72e0
mikujo <jo.tomorrow.0405@gmail.com>
2022-10-25 17:58:17 +0900

    emitによる親要素への操作受け渡し

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -3,23 +3,25 @@ import {ref} from 'vue'
 import TweetPostForm from './TweetPostForm.vue';
 import TweetList from './TweetList.vue';
 const tweets = ref([{id: 0, description: 'Hello world'}, {id: 1, description: 'this is the second'}])
-const inputtingDescription = ref<string>('')
 
-const postTweet = () => {
-  const tweet = {id: Math.random(), description: inputtingDescription.value}
+const postTweet = (description: string) => {
+  const tweet = {id: Math.random(), description}
   tweets.value.push(tweet)
-  inputtingDescription.value = ''
+}
+
+const deleteTweet = (id: number) => {
+  tweets.value = tweets.value.filter(t => t.id !== id)
 }
 </script>
 
 <template>
   <div class="container">
     <h1>Tweeter</h1>
-    <TweetPostForm />
+    <TweetPostForm @post-tweet="postTweet" />
     <div class="tweet-container">
       <p v-if="tweets.length <= 0">No tweets have been added</p>
       <ul>
-        <TweetList :tweets="tweets" />
+        <TweetList :tweets="tweets" @delete-tweet="deleteTweet" />
       </ul>
     </div>
   </div>
--- src/components/TweetList.vue
+++ src/components/TweetList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, defineEmits } from 'vue';
 
 type Tweet = {
   id: number,
@@ -10,8 +10,9 @@ type Props = {
   tweets: Tweet[]
 }
 
+const emit = defineEmits(['delete-tweet'])
 const deleteTweet = (id: number) => {
-  console.log('delete');
+  emit('delete-tweet', id)
 }
 
 defineProps<Props>()

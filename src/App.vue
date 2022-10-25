3385f04cb05a67fc892161952e3543c14308505e
mikujo <jo.tomorrow.0405@gmail.com>
2022-10-25 10:53:36 +0900

    componentsの切り出し

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import PyamentVue from './components/Payment.vue'
+import Tweet from './components/Tweet.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue + typescript" />
+  <Tweet />
+  <!-- <PyamentVue /> -->
+  <!-- <HelloWorld msg="Vite + Vue + typescript" /> -->
 </template>
 
+<!-- scoped = 他のVueファイルと共有しない -->
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/Tweet.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+import TweetPostForm from './TweetPostForm.vue';
+const tweets = ref([{id: 0, description: 'Hello world'}, {id: 1, description: 'this is the second'}])
+const inputtingDescription = ref<string>('')
+
+const postTweet = () => {
+  const tweet = {id: Math.random(), description: inputtingDescription.value}
+  tweets.value.push(tweet)
+  inputtingDescription.value = ''
+}
+
+const deleteTweet = (id: number) => {
+  tweets.value = tweets.value.filter(t => t.id !== id)
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <TweetPostForm />
+    <div class="tweet-container">
+      <p v-if="tweets.length <= 0">No tweets have been added</p>
+      <ul>
+        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+          <span>{{ tweet.description }}</span>
+          <button @click="deleteTweet(tweet.id)" class="delete-button">delete</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.tweet-list {
+  list-style: none;
+  margin-bottom: 12px;
+  border-radius: 4px;
+  font-size: 12px;
+  display: flex;
+  justify-content: space-between;
+  background-color: rgb(204, 219, 233);
+  padding: 8px 20px;
+  width: 300px;
+}
+
+.delete-button {
+color: #fff;
+font-weight: bold;
+background-color: #c99a68;
+border-radius: 2px;
+border: none;
+width: 60px;
+height: 22px;
+}
+
+.delete-button:hover {
+  background-color: #ac783f;
+}
+</style>

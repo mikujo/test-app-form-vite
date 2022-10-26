07ee3b47b274f336db3411470c1eba79b6c1bf49
mikujo <jo.tomorrow.0405@gmail.com>
2022-10-26 14:51:13 +0900

    cardの作成

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,45 @@
 import HelloWorld from './components/HelloWorld.vue'
 import PyamentVue from './components/Payment.vue'
 import Tweet from './components/Tweet.vue'
+import CardList from './components/CardList.vue';
+import SecondCardList from './components/SecondCardList.vue';
+import { ref, computed } from 'vue'
+
+const currentTab = ref('')
+const updateTab = (tabName: string) => {
+  currentTab.value = tabName
+}
+
+const currentComponent = computed(() => {
+  switch (currentTab.value) {
+    case 'CardList':
+      return CardList
+    case 'SecondCardList':
+      return SecondCardList
+    default:
+      return CardList
+  }
+})
 </script>
 
 <template>
-  <Tweet />
+  <div class="tab-changer">
+    <button @click="updateTab('CardList')">Tab 1</button>
+    <button @click="updateTab('SecondCardList')">Tab 2</button>
+  </div>
+  <div class="tab-contents">
+    <component :is="currentComponent"></component>
+    <!-- <CardList v-if="isFirstTab"></CardList>
+    <SecondCardList v-if="!isFirstTab"></SecondCardList> -->
+  </div>
+  <!-- <Tweet /> -->
   <!-- <PyamentVue /> -->
   <!-- <HelloWorld msg="Vite + Vue + typescript" /> -->
 </template>
 
 <!-- scoped = 他のVueファイルと共有しない -->
 <style scoped>
+.tab-contents {
+  margin-top: 20px;
+}
 </style>
---
+++ src/components/RocketIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="rocket">
+
+  </div>
+</template>
+
+<style scoped>
+.rocket {
+  background-color: aqua;
+  width: 50px;
+  height: 50px;
+  margin: 1px;
+}
+</style>

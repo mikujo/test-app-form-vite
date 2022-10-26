8a7d32632f3b1a2848851c9642887b151bd658ff
mikujo <jo.tomorrow.0405@gmail.com>
2022-10-26 14:59:42 +0900

    全タブの表示

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,43 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import PyamentVue from './components/Payment.vue'
+import Payment from './components/Payment.vue'
 import Tweet from './components/Tweet.vue'
 import CardList from './components/CardList.vue';
 import SecondCardList from './components/SecondCardList.vue';
 import { ref, computed } from 'vue'
 
 const currentTab = ref('')
-const updateTab = (tabName: string) => {
+const selectTab = (tabName: string) => {
   currentTab.value = tabName
 }
 
 const currentComponent = computed(() => {
   switch (currentTab.value) {
+    case 'HelloWorld':
+      return HelloWorld
+    case 'Payment':
+      return Payment
+    case 'Tweet':
+      return Tweet
     case 'CardList':
       return CardList
     case 'SecondCardList':
       return SecondCardList
     default:
-      return CardList
+      return HelloWorld
   }
 })
 </script>
 
 <template>
   <div class="tab-changer">
-    <button @click="updateTab('CardList')">Tab 1</button>
-    <button @click="updateTab('SecondCardList')">Tab 2</button>
+    <button @click="selectTab('HelloWorld')">HelloWorld</button>
+    <button @click="selectTab('Payment')">Payment</button>
+    <button @click="selectTab('Tweet')">Tweet</button>
+    <button @click="selectTab('CardList')">CardList1</button>
+    <button @click="selectTab('SecondCardList')">CardList2</button>
+    <button @click="selectTab('TodoList')">TodoList</button>
   </div>
   <div class="tab-contents">
     <component :is="currentComponent"></component>
